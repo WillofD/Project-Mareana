cd8df4f940a4e61c2b8f6076431e0fb5acb0a6c3
unknown <divakar7779@gmail.com>
2023-12-06 12:09:16 +0530

    corretion of drag

--- src/Component/DragTask.js
+++ src/Component/DragTask.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 
-const SortableCard = SortableElement(({ text }) => (
+const SortableCard = SortableElement(({ name, imageUrl }) => (
   <div className="bg-white p-4 shadow-md rounded-md mb-4 cursor-move">
-    <p>{text}</p>
+    <img src={imageUrl} alt={name} className="w-full h-auto mb-2" />
+    <p>{name}</p>
   </div>
 ));
 
-const SortableEmptyTab = SortableContainer(({ cards }) => (
+const SortableEmptyTab = SortableContainer(({ items }) => (
   <div className="flex flex-wrap justify-center">
-    {cards.map((item, index) => (
-      <SortableCard key={index} index={index} text={`Card ${item}`} />
+    {items.map((item, index) => (
+      <SortableCard key={index} index={index} name={item.name} imageUrl={item.imageUrl} />
     ))}
   </div>
 ));
 
-const DragTask = () => {
-  const cards = [1, 2, 3, 4, 5, 6];
+const EmptyTab = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(
+          'https://source.unsplash.com/collection/1163637/300x200/?sig=' + new Date().getTime()
+        );
+        const imageUrl = response.url;
+        setItems(Array.from({ length: 6 }, (_, index) => ({ name: `Player ${index + 1}`, imageUrl })));
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []); 
 
   const onSortEnd = ({ oldIndex, newIndex }) => {
-    console.log(`Move card from ${oldIndex} to ${newIndex}`);
+    setItems(arrayMove(items, oldIndex, newIndex));
   };
 
-  return <SortableEmptyTab cards={cards} onSortEnd={onSortEnd} axis="xy" />;
+  const arrayMove = (arr, oldIndex, newIndex) => {
+    const newArray = [...arr];
+    newArray.splice(newIndex, 0, newArray.splice(oldIndex, 1)[0]);
+    return newArray;
+  };
+
+  return <SortableEmptyTab items={items} onSortEnd={onSortEnd} axis="xy" />;
 };
 
-export default DragTask;
+export default EmptyTab;
